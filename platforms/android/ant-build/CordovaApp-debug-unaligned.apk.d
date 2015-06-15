4b1c1760dd8e3f60e0a4c42233cdb372c1dc5581
abuzerdurmus <abuzer.durmus02@gmail.com>
2015-06-15 19:25:36 +0300

    son iki

---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\workspace\mavikentbilisim\gbmaMobileApp\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\workspace\mavikentbilisim\gbmaMobileApp\platforms\android\ant-build\CordovaApp.ap_ \
+C:\workspace\mavikentbilisim\gbmaMobileApp\platforms\android\ant-build\classes.dex \
+C:\workspace\mavikentbilisim\gbmaMobileApp\platforms\android\CordovaLib\ant-build\classes.jar \
